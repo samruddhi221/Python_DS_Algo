dbd772a863cd06f11ea3b603d345547bc41d1097
samruddhi221 <samruddhinarendra.patil@sjsu.edu>
2021-06-29 23:03:56 -0700

    added definitions of private, protected

--- OOP_in_python/encapsulation.py
+++ OOP_in_python/encapsulation.py
@@ -1,5 +1,9 @@
 # single underscore '_': protected members
+# (The members of a class that are declared protected are only accessible to a class derived from it.)
+#
 # double underscore '__': private members
+# (The members of a class that are declared private are accessible within the class only,
+# private access modifier is the most secure access modifier.)
 
 
 class Base:
@@ -13,11 +17,11 @@ class Derived(Base):
         super().__init__()
         print("public var:", self.public_var)
         print("protected var:", self._protected_var)
-        #print("private var:", self.__private_var)
+        print("private var:", self.__private_var)
 
 
 if __name__ == '__main__':
     d = Derived()
-    print(d.public_var)
-    print(d._protected_var)
-    print(d.__private_var)
+    # print(d.public_var)
+    # print(d._protected_var)
+    # print(d.__private_var)
